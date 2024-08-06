583d2713cdd015e9c59c4f0d6d8c5df10c045248
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2024-08-06 14:18:21 +0700

    [FEAT] - Dashboard: Check role base

--- src/app/[lang]/dashboard/page.tsx
+++ src/app/[lang]/dashboard/page.tsx
@@ -1,4 +1,26 @@
+import { IPayloadJWT } from '@/interfaces/jwt';
+import { jwtDecode } from 'jwt-decode';
+import { cookies } from 'next/headers';
+
 const DashboardPage = () => {
+    const cookiesStore = cookies();
+    const accessToken = cookiesStore.get('accessToken')?.value;
+    const refreshToken = cookiesStore.get('refreshToken')?.value;
+
+    if (!accessToken || !refreshToken) {
+        return <div>Unauthorized</div>;
+    }
+
+    try {
+        const payload: IPayloadJWT = jwtDecode(accessToken);
+
+        if (!payload.userTypes.includes('admin')) {
+            return <div>Forbidden </div>;
+        }
+    } catch (error) {
+        return <div>Unauthorized</div>;
+    }
+
     return <div>Dashboard Page</div>;
 };
 
