129e343cd6c52cf72a2a10bd7e7d429008e516d0
Hà Anh Thảo <thaoanhhaa1@gmail.com>
2024-12-18 23:46:15 +0700

    [FEAT] Filter in payment page

--- src/app/(base)/payments/payments.tsx
+++ src/app/(base)/payments/payments.tsx
@@ -1,44 +1,197 @@
 'use client';
 
 import Payment from '@/app/(base)/payments/payment';
-import { ITransaction } from '@/interfaces/transaction';
-import { getTransactionStatusCode, getTransactionStatusText } from '@/lib/utils';
+import PriceInput from '@/components/input/price-input';
+import TableFilter from '@/components/table-filter';
+import { IProperty } from '@/interfaces/property';
+import { ITransaction, TransactionStatus, TransactionType } from '@/interfaces/transaction';
+import { IBaseUser } from '@/interfaces/user';
+import { getPropertiesByRenterService, getUsersByRenterService } from '@/services/contract-service';
 import { getTransactionsByRenter } from '@/services/transaction-service';
-import { Col, Empty, Flex, Row, Segmented, Spin } from 'antd';
+import { Button, Col, Empty, Flex, Form, Input, Row, Select, Spin } from 'antd';
+import { useForm } from 'antd/es/form/Form';
+import { Filter } from 'lucide-react';
 import { useCallback, useEffect, useState } from 'react';
 
+export interface IFilterPayment {
+    contractId?: string;
+    propertyTitle?: string;
+    ownerName?: string;
+    amount?: number;
+    endDate?: string;
+    type?: TransactionType;
+    status?: TransactionStatus;
+}
+
+const propertyFilterOption = (input: string, option: IProperty | undefined) =>
+    (option?.title ?? '').toLowerCase().includes(input.toLowerCase());
+
+const userFilterOption = (input: string, option: any) =>
+    (option?.name ?? '').toLowerCase().includes(input.toLowerCase());
+
 const Payments = () => {
-    const [segmented, setSegmented] = useState<string>('Tất cả');
     const [transactions, setTransactions] = useState<Array<ITransaction>>([]);
     const [loading, setLoading] = useState<boolean>(true);
-
-    const handleChangeSegmented = (value: string) => {
-        setSegmented(value);
-    };
+    const [activeFilter, setActiveFilter] = useState<boolean>(false);
+    const [form] = useForm();
+    const [owners, setOwners] = useState<Array<IBaseUser>>([]);
+    const [properties, setProperties] = useState<Array<IProperty>>([]);
+    const [ownerLoading, setOwnerLoading] = useState<boolean>(true);
+    const [propertyLoading, setPropertyLoading] = useState<boolean>(true);
 
     const fetchTransactions = useCallback(async () => {
         setLoading(true);
         setTransactions([]);
 
         try {
-            const status = getTransactionStatusCode(segmented);
-
-            const transactions = await getTransactionsByRenter(status);
+            const transactions = await getTransactionsByRenter({
+                ...form.getFieldsValue(),
+            });
 
             setTransactions(transactions);
         } catch (error) {
         } finally {
             setLoading(false);
         }
-    }, [segmented]);
+    }, [form]);
+
+    const handleToggleFilter = () => {
+        setActiveFilter((prev) => !prev);
+    };
+
+    const handleFilter = () => {
+        fetchTransactions();
+    };
+
+    const handleReset = () => {
+        form.resetFields();
+        fetchTransactions();
+    };
 
     useEffect(() => {
         fetchTransactions();
     }, [fetchTransactions]);
 
+    useEffect(() => {
+        const fetchOwnerCbb = async () => {
+            try {
+                const owners = await getUsersByRenterService();
+
+                console.log(owners.map((owner) => ({ ...owner, name: owner.name + ' - ' + owner.email })));
+
+                setOwners(owners.map((owner) => ({ ...owner, name: owner.name + ' - ' + owner.email })));
+            } catch (error) {
+            } finally {
+                setOwnerLoading(false);
+            }
+        };
+
+        const fetchPropertyCbb = async () => {
+            try {
+                const properties = await getPropertiesByRenterService();
+
+                setProperties(properties);
+            } catch (error) {
+            } finally {
+                setPropertyLoading(false);
+            }
+        };
+
+        fetchOwnerCbb();
+        fetchPropertyCbb();
+    }, []);
+
     return (
         <div>
-            <div className="flex justify-end my-3">
+            <Flex gap="small" justify="flex-end" wrap className="my-4">
+                <Button
+                    type={activeFilter ? 'primary' : 'default'}
+                    className="ml-auto"
+                    icon={<Filter className="w-5 h-5" />}
+                    onClick={handleToggleFilter}
+                />
+            </Flex>
+            {activeFilter && (
+                <TableFilter<IFilterPayment> onFinish={handleFilter} onReset={handleReset} form={form}>
+                    <Col span={8}>
+                        <Form.Item label="Mã hợp đồng" name="contractId">
+                            <Input allowClear placeholder="Nhập mã hợp đồng" />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Bất động sản" name="propertyId">
+                            <Select
+                                placeholder="Chọn bất động sản"
+                                options={properties}
+                                fieldNames={{ label: 'title', value: 'propertyId' }}
+                                loading={propertyLoading}
+                                allowClear
+                                showSearch
+                                filterOption={propertyFilterOption}
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Chủ nhà" name="ownerId">
+                            <Select
+                                allowClear
+                                showSearch
+                                filterOption={userFilterOption}
+                                placeholder="Chọn chủ nhà"
+                                options={owners}
+                                loading={ownerLoading}
+                                fieldNames={{ label: 'name', value: 'userId' }}
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Số tiền" name="amount">
+                            <PriceInput placeholder="Nhập số tiền" />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Loại" name="type">
+                            <Select
+                                allowClear
+                                placeholder="Chọn loại"
+                                options={[
+                                    {
+                                        label: 'Tiền cọc',
+                                        value: 'DEPOSIT',
+                                    },
+                                    {
+                                        label: 'Tiền thuê',
+                                        value: 'RENT',
+                                    },
+                                ]}
+                            />
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Trạng thái" name="status">
+                            <Select
+                                allowClear
+                                placeholder="Chọn trạng thái"
+                                options={[
+                                    {
+                                        label: 'Chờ thanh toán',
+                                        value: 'PENDING',
+                                    },
+                                    {
+                                        label: 'Thành công',
+                                        value: 'COMPLETED',
+                                    },
+                                    {
+                                        label: 'Đã hủy',
+                                        value: 'CANCELLED',
+                                    },
+                                ]}
+                            />
+                        </Form.Item>
+                    </Col>
+                </TableFilter>
+            )}
+            {/* <div className="flex justify-end my-3">
                 <Segmented<string>
                     options={[
                         'Tất cả',
@@ -48,7 +201,7 @@ const Payments = () => {
                     ]}
                     onChange={handleChangeSegmented}
                 />
-            </div>
+            </div> */}
             <Row gutter={[12, 12]}>
                 {transactions.map((transaction) => (
                     <Col key={transaction.id} span={12}>
